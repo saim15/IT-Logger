495e2fcbc18545948dc5d929c065acffc665678a
Sai Manikonda <saimanikonda@gmail.com>
2020-05-03 23:36:13 -0500

    first commit & initial version 1

---
+++ src/actions/techActions.js
@@ -0,0 +1,81 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  SET_LOADING,
+  TECHS_ERROR,
+} from "./types";
+
+//Get techs from server
+export const getTechs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const result = await fetch("/techs");
+    const data = await result.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
+//Add techs from server
+export const addTech = (tech) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const result = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await result.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
+//Delete techs from server
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
+//Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
